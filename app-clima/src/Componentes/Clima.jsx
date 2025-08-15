c6207b4c87908ac0b8750633fbafb9d30aade5a5
Lucas Sorzio <sorziolucas4@gmail.com>
2025-08-15 16:42:03 -0300

    pequeña mejora

--- app-clima/src/Componentes/Clima.jsx
+++ app-clima/src/Componentes/Clima.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 const Clima = ({ temp, temp_min, temp_max, humedad, weather }) => {
+    
+    const obtenerEmogi = (temp) => {
+        if (temp > 25) {
+            return "🌞"
+        }
+        if (temp > 15 && temp <= 25) {
+            
+            return "🌤️"
+        }
+        else {
+            return "❄️";
+        }
+      
+
+
+
+    }
+    
+    
     return (
         <div className={temp > 20 ?' bg-linear-to-r from-amber-300 to-orange-600 m-5 p-8 rounded-lg shadow- text-black border-amber-400 border-2' : 'bg-linear-to-r from-blue-700 to-blue-400 m-5 p-8 rounded-lg shadow- text-white border-blue-400 border-3'} >
-          <p>Temperatura : <b>{ temp}</b></p>
+           {obtenerEmogi(temp)}<p>Temperatura : <b>{ temp}</b></p>
           <p>Temperatura Max : <b>{temp_min}</b></p>
           <p>Temperatura Min : <p>Temperatura Max : <b>{temp_max}</b></p>
 </p>
