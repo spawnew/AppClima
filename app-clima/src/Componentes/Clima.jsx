2a0bb1aaeb11e764fa3a6d365666841a263d319a
Lucas Sorzio <sorziolucas4@gmail.com>
2025-08-15 22:20:08 -0300

    Arreglo de la temperatura minima

--- app-clima/src/Componentes/Clima.jsx
+++ app-clima/src/Componentes/Clima.jsx
@@ -23,9 +23,9 @@ const Clima = ({ temp, temp_min, temp_max, humedad, weather }) => {
     return (
         <div className={temp > 20 ?' bg-linear-to-r from-amber-300 to-orange-600 m-5 p-8 rounded-lg shadow- text-black border-amber-400 border-2' : 'bg-linear-to-r from-blue-700 to-blue-400 m-5 p-8 rounded-lg shadow- text-white border-blue-400 border-3'} >
            {obtenerEmogi(temp)}<p>Temperatura : <b>{ temp}°C</b></p>
-          <p>Temperatura Max : <b>{temp_min}°C</b></p>
-          <p>Temperatura Min : <p>Temperatura Max : <b>{temp_max}°C</b></p>
-</p>
+          <p>Temperatura Min : <b>{temp_min}°C</b></p>
+          <p>Temperatura Max :  <b>{temp_max}°C</b></p>
+
             <p className={humedad > 50 ? 'text-blue-850 text-2md ' : 'text-black'}>Humedad  <b>{ humedad}</b> %</p>
           <p>Clima : <b>{weather}</b></p>
            
